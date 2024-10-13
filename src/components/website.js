93c8b2c8d165f4cf04282b7f34d442881b7c36fb
unknown <ashley.wu@stonybrook.edu>
2024-10-12 21:19:54 -0400

    Creating banner styling

--- src/components/website.js
+++ src/components/website.js
@@ -10,3 +10,17 @@ export const header = () =>{
         </div>
     )
 }
+
+/*
+<html>
+<head>
+  <link rel="stylesheet" href="css/index.css">
+  <script type="module" src="src/index.js"></script>
+  <title>Ashley Wu's Personal Website</title>
+  <link rel="icon" type="image/x-icon" href="image\Official Phreddit Logo.png">
+</head>
+<body class="body">
+    <h3 id="welcome">Welcome to my Website!</h3>
+</body>
+</html>
+*/
